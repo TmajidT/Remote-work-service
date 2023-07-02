51fb538bc2f5186416883b0eb0f4770d884814f3
Majid <majid.asfanani@gmail.com>
2023-07-02 14:27:14 +0430

    changed main a bit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import add_users
 
+#all of the abilities ---- you can delete # and use them
 
-add_users.create_or_connect_to_database()
-add_users.add_user()
+
+#add a user
+#add_users.create_or_connect_to_database()
+#add_users.add_user()
